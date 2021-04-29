0fe0a3b396477f5ff27ed88ec7d638503c7e1aab
rasyid-99 <75787747+rasyid-99@users.noreply.github.com>
2021-04-29 23:09:28 +0700

    Add files via upload

---
+++ Classes/detailClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeAndSound.Classes
+{
+    class detailClass
+    {
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public static string Myconnstrng { get => myconnstrng; set => myconnstrng = value; }
+
+        private static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+    }
+}
